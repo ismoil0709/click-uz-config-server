4bb05ca21adc96c0ad1b8ca3694d46f34103f99a
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-02-12 19:45:50 +0500

    application yml added

---
+++ src/main/java/uz/pdp/clickuzconfigserver/ClickUzConfigServerApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.clickuzconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickUzConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickUzConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/clickuzconfigserver/ClickUzConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.clickuzconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickUzConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
