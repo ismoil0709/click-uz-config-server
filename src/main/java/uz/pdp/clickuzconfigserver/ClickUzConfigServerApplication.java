748e17842da1b61246e3dd6443ea4833f8d88fec
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-02-12 20:15:48 +0500

    @EnableConfigServer added

--- src/main/java/uz/pdp/clickuzconfigserver/ClickUzConfigServerApplication.java
+++ src/main/java/uz/pdp/clickuzconfigserver/ClickUzConfigServerApplication.java
@@ -2,7 +2,9 @@ package uz.pdp.clickuzconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ClickUzConfigServerApplication {
 
